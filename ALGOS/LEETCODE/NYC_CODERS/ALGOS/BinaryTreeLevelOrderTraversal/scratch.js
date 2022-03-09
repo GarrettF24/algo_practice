03890a39778631c449a5d8b02ca2f0fdd7a8bec6
GarrettF <grfoster24@gmail.com>
2022-03-08 19:59:58 -0500

    deleted some lines

--- ALGOS/LEETCODE/NYC_CODERS/ALGOS/BinaryTreeLevelOrderTraversal/scratch.js
+++ ALGOS/LEETCODE/NYC_CODERS/ALGOS/BinaryTreeLevelOrderTraversal/scratch.js
@@ -10,34 +10,6 @@
 const levelOrder = (root) => {
   // create a queue
   let queue = [];
-  // add the root node to the queue
-  queue.push(root);
-  // while queue is not empty
-  while (queue.length) {
-    //Store the current size of queue
-    const currLevelSize = queue.length;
-    // for each node in the queue (current level)
-    for (let i = 0; i < currLevelSize; i++) {
-      const node = queue.shift();
-      // perform operation(s) with node
-      console.log(node.val);
-      // if node has a left node
-      if (node.left !== null) {
-        // add left node to the queue
-        queue.push(node.left);
-      }
-      // if node has a right node
-      if (node.right !== null) {
-        // add right node to the queue
-        queue.push(node.right);
-      }
-    }
-  }
-};
-
-const levelOrder = (root) => {
-  // create a queue
-  let queue = [];
   //create array to hold each row
   let result = [];
   //push root into the queue
